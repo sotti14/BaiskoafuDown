d21253fe0b011490d365e27253821818142d6d27
sotti14 <82983398+sotti14@users.noreply.github.com>
2021-04-22 04:58:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Baiskoafu loagin details
-username = ""
-password = ""
+username = "sotti"
+password = "1234567890,,"
 
-def media_quality(quality='medium'):
+def media_quality(quality='high'):
 
     q = ['high', 'low', 'medium']
 
@@ -12,5 +12,5 @@ def media_quality(quality='medium'):
     return q[2] # < --- default is medium
 
 media_quality() # ['high', 'low', 'medium']
-ASK_BEFORE_DOWNLOAD = True	# set 'False' for automatic download
-IS_PRIMARY_DEVICE   = True	# set 'True' only if you have premium subscription
+ASK_BEFORE_DOWNLOAD = False	# set 'False' for automatic download
+IS_PRIMARY_DEVICE   = False	# set 'True' only if you have premium subscription
